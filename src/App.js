4448e07ebd3828c0d5dfc35a191d5c0ea80a363a
oneway <oneway@codemind.co.kr>
2024-05-21 17:52:10 +0900

    03. 리액트 기초 개념 알아보기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function Food(props) {
+  console.log(props);
+  return <h1>I like { props.fav }</h1>
+}
+
 function App() {
-  return <div>Hello!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="rame" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+    </div>
+  )
 }
 
 export default App;
